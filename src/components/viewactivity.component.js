6c771542c12d23b11d2ddb68b70d86b38cf177d0
MichaelSault <michael_sault@hotmail.com>
2021-12-22 03:10:14 -0500

    css changes to keep the page from clipping the top of the browser

--- src/components/viewactivity.component.js
+++ src/components/viewactivity.component.js
@@ -20,12 +20,13 @@ export default class ViewActivity extends Component {
         return (
             <div>
                 <h1>Recent Activities:</h1>
-                { this.state.activities.map(activity => (<div key={activity.id} className = "activitybox"><h4>{activity.title}</h4>
+                { this.state.activities.slice(0,3).map(activity => (<div key={activity.id} className = "activitybox"><h4>{activity.title}</h4>
                                                             <pbot><b>Date: </b>{activity.date} at {activity.start_time} <br></br></pbot>
                                                             <pbot><b>Distance: </b>{activity.distance}km&emsp;<b>Time: </b>{activity.time_hours}:{activity.time_minutes}:{activity.time_seconds}</pbot>
                                                             <p><b>Elevation: </b>{activity.elevation}m</p>
                                                             <p><i>{activity.description}</i></p>
                                                             <p><b>Tags: </b>{activity.sport}, {activity.type}</p>
+                                                            <button>View</button><button>Edit</button><button>Delete</button>
                                                         </div>)) }
             </div>
         );
