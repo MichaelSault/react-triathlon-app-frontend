1590bc50ac2517018410488b005ea40aa45ca559
MichaelSault <michael_sault@hotmail.com>
2022-01-05 14:29:48 -0500

    Updates

---
+++ precache-manifest.b43844f497a1630f32120bbd964877b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cacdacb20414a8b2982e6521710e794",
+    "url": "/react-triathlon-app-frontend/index.html"
+  },
+  {
+    "revision": "f14a95f1a05a1ee87273",
+    "url": "/react-triathlon-app-frontend/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "a309f77e17fcb24eab17",
+    "url": "/react-triathlon-app-frontend/static/css/main.298c728d.chunk.css"
+  },
+  {
+    "revision": "f14a95f1a05a1ee87273",
+    "url": "/react-triathlon-app-frontend/static/js/2.1806b7b5.chunk.js"
+  },
+  {
+    "revision": "a309f77e17fcb24eab17",
+    "url": "/react-triathlon-app-frontend/static/js/main.0fd9b692.chunk.js"
+  },
+  {
+    "revision": "dd3806e3e4bfb60f6794",
+    "url": "/react-triathlon-app-frontend/static/js/runtime-main.aeb80148.js"
+  }
+]);
